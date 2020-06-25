f64e017c3cdb616a428a852f75fa63602cc123c4
Juani Gallo <ji.gallo93@gmail.com>
2020-06-25 17:17:54 -0300

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import {shallow} from 'enzyme'
+import { isElementOfType } from 'react-dom/test-utils';
 
-test('renders learn react link', () => {
+it('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+describe('P in App', () => {
+
+  it('Check if first P is CourseIt', () => {
+    const wrapper = shallow(<App />)
+    const text = wrapper.find('#courseit-test').text()
+  
+    expect(text).toBe('CourseIt')
+  })
+  
+  it('App has only one P', () => {
+    const wrapper = shallow(<App />)
+    const p = wrapper.find('p')
+    expect(p).toHaveLength(2)
+  })
+})
